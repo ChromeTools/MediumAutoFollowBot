421b78f138d248b1e766c112142a28ecacc1c9b3
Chrome Tools <mediumtooldev@gmail.com>
2020-09-19 20:10:23 -0700

    adding the add all to whitelist button to the followers page as well, resizing the whitelist form

--- js/followers.js
+++ js/followers.js
@@ -8,6 +8,30 @@ const followAllButton = document.createElement("button");
 followAllButton.innerText="Follow All";
 followAllButton.className = "button button--chromeless u-baseColor--buttonNormal"
 
+const populateWhitelistButton = document.createElement("button"); 
+populateWhitelistButton.innerText="Add All To Whitelist";
+populateWhitelistButton.className = "button button--chromeless u-baseColor--buttonNormal"
+
+
+populateWhitelistButton.onclick = async () => {
+	await scroll(numberOfScrolls, 1000) // 1 second between scrolls
+
+	const userAliases = $.map($(USER_PROFILE_SELECTOR), user => {
+		return $(user).attr('href').split('/')[USER_NAME_HREF_INDEX]
+	})
+    
+    whitelist = new Set(await getLocalObj(UNFOLLOW_WHITELIST) || [])
+
+	diff = userAliases.filter(alias => !whitelist.has(alias))
+
+	clearButterBarMessages()
+	appendButterBarMessage(`Adding ${diff.length} new users to your whitelist.`)
+
+    whitelist = [...whitelist].concat(diff)
+    await setLocalObj(UNFOLLOW_WHITELIST, whitelist)
+}
+
+
 followAllButton.onclick = async () => {
 	await scroll(numberOfScrolls, 2000) // 2 seconds between scrolls
 
@@ -34,6 +58,10 @@ var followerCountButton = $(FOLLOWER_COUNT_SELECTOR)
 var followingCountButton = $(FOLLOWING_COUNT_SELECTOR)
 numberOfScrolls = Math.ceil((followerCountButton[0].getAttribute('title').split(/\s+/)[1].replace(/,/g, '') - 18) / 11)
 followerCountButton[0].after(followAllButton)
+
+//all this just to insert the "populate white list" button after the "follow all" button...
+followAllButton.parentNode.insertBefore(populateWhitelistButton, followAllButton.nextSibling)
+
 //we had to add this hack to ensure the page is reloaded when the following count button is clicked, 
 // otherwise the following.js script doesn't get matched against the URL since chrome doesn't know that a URL change has actually occurred.
 followingCountButton.click(() => {
